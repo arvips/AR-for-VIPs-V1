e1e48f1278b6c38bea87776ae916d997aac6c202
bwubrian <bwubrian@gmail.com>
2019-04-07 15:47:58 -0700

    Control Script Implemented
    In Control Test scene, Script Manager has new script Control Script which allows activation of all 3 core commands (Obstacles, Locate Text, Read Text) with voice commands or gestures (tap, hold, and manipulate).

---
+++ Assets/Scripts/ControlScript.cs
@@ -0,0 +1,141 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity;
+using HoloToolkit.Unity.InputModule;
+using UnityEngine.UI;
+using UnityEngine.XR.WSA.Input;
+using System;
+
+/// <summary>
+/// Control Script should take in inputs (primarily voice) and use them to trigger the three main commands: Obstacles, Locate Text, and Read Text.
+/// Which method each of the three commands maps to should be adjustable by adjusting parameters in the Control Script's insepctor.
+/// For example, it should be easy to have "Obstacles" use shoot beacon, spray beacon, or boxcast spray beacon.
+/// This should make it easier to test various scripts and incorporate them into the prototype.
+/// </summary>
+
+[System.Serializable]
+public class ControlScript : MonoBehaviour {
+
+    //Help field in inspector
+    [SerializeField]
+#if UNITY_EDITOR
+    [Help("\nHOW TO USE CONTROL SCRIPT\n\n1. Attach relevent scripts to the Script Manager\n\n2. Open Control Script in editor\n\n3. Adjust the Core Commands (Obstacles, LocateText, and ReadText functions) to use the attached scripts\n", UnityEditor.MessageType.Info)]
+#endif
+    float inspectorField = 1440f; //without this, help field goes away
+
+    #region **INITIALIZATION**
+
+    public GestureRecognizer gestureRec;
+    public GameObject obstacleBeaconManager;
+    public GameObject textBeaconManager;
+
+    void Start () {
+
+        //Start a gesture recognizer to look for the Tap gesture
+        gestureRec = new GestureRecognizer();
+
+        gestureRec.Tapped += Tap;
+        gestureRec.HoldStarted += HoldStarted;
+        gestureRec.ManipulationStarted += ManipulationStarted;
+
+        gestureRec.SetRecognizableGestures(GestureSettings.Tap);
+        //gestureRec.SetRecognizableGestures(GestureSettings.DoubleTap);
+        gestureRec.SetRecognizableGestures(GestureSettings.Hold);
+        gestureRec.SetRecognizableGestures(GestureSettings.ManipulationTranslate);
+
+        gestureRec.StartCapturingGestures();
+        Debug.Log("Gesture Recognizer Initialized");
+
+    }
+
+    void OnDisable()
+    {
+        //unsubscribe from event
+        gestureRec.Tapped -= Tap;
+        gestureRec.HoldStarted -= HoldStarted;
+        gestureRec.ManipulationStarted -= ManipulationStarted;
+    }
+
+    #endregion
+
+
+    #region **GESTURE CONTROLS**
+    public void Tap(TappedEventArgs args) //(InteractionSourceKind source, int tapCount, Ray headRay) //(InteractionSource source, InteractionSourcePose sourcePose, Pose headPose, int tapCount) 
+    {
+        //On Tap, activate "obstacles" command
+        Obstacles();
+
+        Debug.Log("Tap event registered");
+    }
+
+    public void HoldStarted (HoldStartedEventArgs args)
+    {
+        //On Hold, activate "Locate Text" command
+        LocateText();
+        Debug.Log("Hold Started event registered");
+    }
+
+    public void ManipulationStarted (ManipulationStartedEventArgs args)
+    {
+        //On Manipulation (tap and move), activate "Read Text" command
+        ReadText();
+        Debug.Log("Manipulation Started event registered");
+    }
+
+    #endregion
+
+
+    //**ADD CORE COMMANDS TO OTHER SCRIPTS ATTACHED TO CONTROL SCRIPT HERE**
+    #region **CORE COMMANDS**
+
+    public void Obstacles ()
+    {
+        Debug.Log("Obstacles command activated");
+
+        GetComponentInParent<ShootBeacon>().SingleShot();
+
+    }
+
+    public void LocateText ()
+    {
+        Debug.Log("Locate Text command activated");
+
+
+    }
+
+    public void ReadText ()
+    {
+        Debug.Log("Read Text command activated");
+
+    }
+
+    #endregion
+
+
+    //OTHER FUNCTIONS
+    #region **CLEAR BEACONS**
+
+    public void ClearObstacleBeacons ()
+    {
+        //Destroys all obstacle beacons.
+        foreach (Transform child in obstacleBeaconManager.transform)
+        {
+            Destroy(child.gameObject);
+        }
+    }
+
+    public void ClearTextBeacons ()
+    {
+        //Destorys all text beacons.
+        foreach (Transform child in textBeaconManager.transform)
+        {
+            Destroy(child.gameObject);
+        }
+    }
+
+    #endregion
+
+
+
+}
--- Assets/Scripts/ShootBeacon.cs
+++ Assets/Scripts/ShootBeacon.cs
@@ -5,40 +5,43 @@ using HoloToolkit.Unity;
 using HoloToolkit.Unity.InputModule;
 using HoloToolkit.Unity.SpatialMapping;
 
-public class ShootBeacon : MonoBehaviour, IInputClickHandler {
+public class ShootBeacon : MonoBehaviour {
 
-    public GameObject beacon; 
+    public GameObject beacon;
+    public GameObject beaconManager;
 
 	// Use this for initialization
 	void Start () {
-        Debug.Log("ShootBeacon OnStart Triggered");
-        //SpatialMapping.Instance.DrawVisualMeshes = true;
+        //Debug.Log("ShootBeacon OnStart Triggered");
     }
 
 
-    public void OnInputClicked(InputClickedEventData eventData)
-    {
-        Debug.Log("OnInputClicked Triggered");
+    // Update is called once per frame
+    void Update () {
 
-        
-        // Do a raycast into the world that will only hit the Spatial Mapping mesh.
+	}
+
+    public void SingleShot ()
+    {
+        // Do a raycast straight out from the camera.
         var headPosition = Camera.main.transform.position;
         var gazeDirection = Camera.main.transform.forward;
 
 
-        
+
         RaycastHit hitInfo;
 
         if (Physics.Raycast(headPosition, gazeDirection, out hitInfo,
             30.0f))
         {
+            //Debug
             //Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hitInfo.distance, Color.yellow);
-            Debug.Log("Did Hit");
-            Debug.Log("Hit point: " + hitInfo.point);
-            Debug.Log("Hit transform: " + hitInfo.transform);
+            //Debug.Log("Did Hit");
+            Debug.Log("Beacon hit at location: " + hitInfo.point);
+            //Debug.Log("Hit transform: " + hitInfo.transform);
 
             //Instantiate Beacon
-            GameObject beaconClone = Instantiate(beacon, hitInfo.point, Quaternion.identity);
+            Instantiate(beacon, hitInfo.point, Quaternion.identity, beaconManager.transform);
 
         }
         else
@@ -46,7 +49,35 @@ public class ShootBeacon : MonoBehaviour, IInputClickHandler {
             Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
             Debug.Log("Did not Hit");
         }
+    }
+}
 
+
+
+/* Backup Script
+ * 
+ * using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity;
+using HoloToolkit.Unity.InputModule;
+using HoloToolkit.Unity.SpatialMapping;
+
+public class ShootBeacon : MonoBehaviour, IInputClickHandler {
+
+    public GameObject beacon; 
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("ShootBeacon OnStart Triggered");
+    }
+
+
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        Debug.Log("OnInputClicked Triggered");
+
+        shootBeacon();
 
         eventData.Use(); // Mark the event as used, so it doesn't fall through to other handlers.
     }
@@ -55,4 +86,35 @@ public class ShootBeacon : MonoBehaviour, IInputClickHandler {
     void Update () {
 
 	}
+
+    public void shootBeacon ()
+    {
+        // Do a raycast into the world that will only hit the Spatial Mapping mesh.
+        var headPosition = Camera.main.transform.position;
+        var gazeDirection = Camera.main.transform.forward;
+
+
+
+        RaycastHit hitInfo;
+
+        if (Physics.Raycast(headPosition, gazeDirection, out hitInfo,
+            30.0f))
+        {
+            //Debug
+            //Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hitInfo.distance, Color.yellow);
+            //Debug.Log("Did Hit");
+            Debug.Log("Beacon hit at location: " + hitInfo.point);
+            //Debug.Log("Hit transform: " + hitInfo.transform);
+
+            //Instantiate Beacon
+            Instantiate(beacon, hitInfo.point, Quaternion.identity);
+
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
+            Debug.Log("Did not Hit");
+        }
+    }
 }
+*/
