38d26696388e2d50244e777bcebd36981bedeebd
Ji <jihan2311@gmail.com>
2019-10-23 17:56:42 -0800

    Created Azure TTS Structure, yet to test

--- Assets/Scripts/Text Recognition/TextReco.cs
+++ Assets/Scripts/Text Recognition/TextReco.cs
@@ -5,8 +5,13 @@ using System.Text;
 using UnityEngine.Networking;
 using System;
 using System.IO;
+using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+//using System.Net.HTTP;
 
-public class TextReco : MonoBehaviour {
+public class TextReco : MonoBehaviour
+{
 
     public IEnumerator GoogleRequest(byte[] image)
     {
@@ -33,14 +38,14 @@ public class TextReco : MonoBehaviour {
         var header = new Dictionary<string, string>() {
             { "Content-Type", "application/json" }
         };
-       // Debug.Log("TR: Request to API (header) sent");
+        // Debug.Log("TR: Request to API (header) sent");
 
         var data = Encoding.UTF8.GetBytes(json);
         //Debug.Log("TR: data encoded.");
 
         WWW www = new WWW(url, data, header);
         //Debug.Log("TR: www created.");
-        
+
 
         // Send API
         yield return www;
@@ -82,9 +87,66 @@ public class TextReco : MonoBehaviour {
             {
                 Debug.Log("TR: www.text length = " + www.text.Length);
             }
-            
+
         }
 
 
+    }
+
+    public async IEnumerator AzureRequest(byte[] image)
+    {
+        string subscriptionKey = "bc12a427cee04549a3014196d8e12078";
+        string endpoint = "https://arvips.cognitiveservices.azure.com/";
+        string uriBase = endpoint + "vision/v2.1/ocr";
+
+        try
+        {
+            HttpClient client = new HttpClient();
+
+            // Request headers.
+            client.DefaultRequestHeaders.Add(
+                "Ocp-Apim-Subscription-Key", subscriptionKey);
+
+            // Request parameters. 
+            // The language parameter doesn't specify a language, so the 
+            // method detects it automatically.
+            // The detectOrientation parameter is set to true, so the method detects and
+            // and corrects text orientation before detecting text.
+            string requestParameters = "language=unk&detectOrientation=true";
+
+            // Assemble the URI for the REST API method.
+            string uri = uriBase + "?" + requestParameters;
+
+            HttpResponseMessage response;
+
+            byte[] byteData = image;
+
+
+            using (ByteArrayContent content = new ByteArrayContent(byteData))
+            {
+                // This example uses the "application/octet-stream" content type.
+                // The other content types you can use are "application/json"
+                // and "multipart/form-data".
+                content.Headers.ContentType =
+                    new MediaTypeHeaderValue("application/octet-stream");
+
+                // Asynchronously call the REST API method.
+                response = await client.PostAsync(uri, content);
+            }
+
+            // Asynchronously get the JSON response.
+            string contentString = await response.Content.ReadAsStringAsync();
+
+            // Indicate when no text is detected
+            if (respJson.Contains("textAnnotations") && !respJson.Contains("error"))
+            {
+                Debug.Log("TR: Text detected.");
+                GetComponent<IconManager>().CreateIcons(respJson);
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("\n" + e.Message);
+        }
     }
 }
